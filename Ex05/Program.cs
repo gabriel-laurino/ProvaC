c09af4411be6094b2fe2209d8b5866ac84a24f7c
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-05-02 09:58:05 -0300

    correção: 'namespace' → 'using'

--- Ex05/Program.cs
+++ Ex05/Program.cs
@@ -1,67 +1,66 @@
-﻿namespace Ex05
+﻿using Ex05;
+
+class Program05
 {
-    class Program05
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            List<Filme> filmes = new List<Filme>
+        List<Filme> filmes = new List<Filme>
         {
             new Filme("Velozes e Furiosos", "Sci-fi", 148),
             new Filme("Dora Aventureira", "Aventura", 136),
             new Filme("Super Aulas de C#", "Terror", 155)
         };
 
-            while (true)
-            {
-                Console.WriteLine("\nEscolha uma opção:");
-                Console.WriteLine("1 - Verificar disponibilidade de um filme");
-                Console.WriteLine("2 - Registrar locação de um filme");
-                Console.WriteLine("3 - Registrar devolução de um filme");
-                Console.WriteLine("4 - Sair");
-                Console.Write("\nDigite sua opção: ");
-                string opcao = Console.ReadLine() ?? "";
+        while (true)
+        {
+            Console.WriteLine("\nEscolha uma opção:");
+            Console.WriteLine("1 - Verificar disponibilidade de um filme");
+            Console.WriteLine("2 - Registrar locação de um filme");
+            Console.WriteLine("3 - Registrar devolução de um filme");
+            Console.WriteLine("4 - Sair");
+            Console.Write("\nDigite sua opção: ");
+            string opcao = Console.ReadLine() ?? "";
 
-                switch (opcao)
-                {
-                    case "1":
-                        Filme? filmeParaVerificar = EscolherFilme(filmes);
-                        filmeParaVerificar?.VerificarDisponibilidade();
-                        break;
-                    case "2":
-                        Filme? filmeParaLocar = EscolherFilme(filmes);
-                        filmeParaLocar?.RegistrarLocacao();
-                        break;
-                    case "3":
-                        Filme? filmeParaDevolver = EscolherFilme(filmes);
-                        filmeParaDevolver?.RegistrarDevolucao();
-                        break;
-                    case "4":
-                        Console.WriteLine("\nPrograma encerrado.");
-                        return;
-                    default:
-                        Console.WriteLine("\nOpção inválida. Tente novamente.");
-                        break;
-                }
+            switch (opcao)
+            {
+                case "1":
+                    Filme? filmeParaVerificar = EscolherFilme(filmes);
+                    filmeParaVerificar?.VerificarDisponibilidade();
+                    break;
+                case "2":
+                    Filme? filmeParaLocar = EscolherFilme(filmes);
+                    filmeParaLocar?.RegistrarLocacao();
+                    break;
+                case "3":
+                    Filme? filmeParaDevolver = EscolherFilme(filmes);
+                    filmeParaDevolver?.RegistrarDevolucao();
+                    break;
+                case "4":
+                    Console.WriteLine("\nPrograma encerrado.");
+                    return;
+                default:
+                    Console.WriteLine("\nOpção inválida. Tente novamente.");
+                    break;
             }
         }
+    }
 
-        static Filme? EscolherFilme(List<Filme> filmes)
+    static Filme? EscolherFilme(List<Filme> filmes)
+    {
+        Console.WriteLine("\nEscolha um filme:");
+        for (int i = 0; i < filmes.Count; i++)
         {
-            Console.WriteLine("\nEscolha um filme:");
-            for (int i = 0; i < filmes.Count; i++)
-            {
-                Console.WriteLine($"\n{i + 1} - {filmes[i].Titulo}");
-            }
-            Console.Write("\nDigite o número do filme: ");
-            if (int.TryParse(Console.ReadLine(), out int indice) && indice > 0 && indice <= filmes.Count)
-            {
-                return filmes[indice - 1];
-            }
-            else
-            {
-                Console.WriteLine("\nOpção de filme inválida.");
-                return null;
-            }
+            Console.WriteLine($"\n{i + 1} - {filmes[i].Titulo}");
+        }
+        Console.Write("\nDigite o número do filme: ");
+        if (int.TryParse(Console.ReadLine(), out int indice) && indice > 0 && indice <= filmes.Count)
+        {
+            return filmes[indice - 1];
+        }
+        else
+        {
+            Console.WriteLine("\nOpção de filme inválida.");
+            return null;
         }
     }
 }
--- Ex06/Program.cs
+++ Ex06/Program.cs
@@ -1,20 +1,19 @@
-﻿namespace Ex06
-{
-    class Program06
-    {
-        static void Main(string[] args)
-        {
-            Carro carro = new Carro("Toyota Corolla", 2021, "Prata", 4);
-            Moto moto = new Moto("Yamaha YZF", 2022, "Azul", 600);
-            Bicicleta bicicleta = new Bicicleta("Caloi Andes", 2020, "Vermelho", 21);
+﻿using Ex06;
 
-            Console.WriteLine();
-            carro.ExibirInformacoes();
-            Console.WriteLine();
-            moto.ExibirInformacoes();
-            Console.WriteLine();
-            bicicleta.ExibirInformacoes();
-            Console.WriteLine();
-        }
+class Program06
+{
+    static void Main(string[] args)
+    {
+        Carro carro = new Carro("Toyota Corolla", 2021, "Prata", 4);
+        Moto moto = new Moto("Yamaha YZF", 2022, "Azul", 600);
+        Bicicleta bicicleta = new Bicicleta("Caloi Andes", 2020, "Vermelho", 21);
+
+        Console.WriteLine();
+        carro.ExibirInformacoes();
+        Console.WriteLine();
+        moto.ExibirInformacoes();
+        Console.WriteLine();
+        bicicleta.ExibirInformacoes();
+        Console.WriteLine();
     }
 }
